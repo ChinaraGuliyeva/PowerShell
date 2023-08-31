3082bcfdd67b7154b3826b790dce1bb8f5bf57fd
Chinara <ch.quliyeva@dxa.gov.az>
2023-08-31 16:48:53 +0400

    Added dialog for initial window

--- Photo_sorting.ps1
+++ Photo_sorting.ps1
@@ -1,12 +1,39 @@
 Add-Type -AssemblyName System.Windows.Forms
 
-$openFolderDialog = New-Object System.Windows.Forms.FolderBrowserDialog
-$openFolderDialog.Description = "Select the source directory"
-$dialogResult = $openFolderDialog.ShowDialog()
-$yearPattern = "20\d{2}"
+function Show-SuccessDialog {
+    $result = [System.Windows.Forms.MessageBox]::Show("Files sorted successfully", "Success", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+}
+function Show-ErrorDialog {
+    $result = [System.Windows.Forms.MessageBox]::Show("An error occurred. Please try again.", "Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+}
 
-if ($dialogResult -eq [System.Windows.Forms.DialogResult]::OK) {
-    $folderPath = $openFolderDialog.SelectedPath
+
+$yearPattern = "20\d{2}"
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Custom Folder Dialog"
+$form.Width = 400
+$form.Height = 200
+
+$label = New-Object System.Windows.Forms.Label
+$label.Text = "Select the source directory:"
+$label.Location = New-Object System.Drawing.Point(10, 20)
+$label.Width = $form.Width - 20
+$label.Font = New-Object System.Drawing.Font("Arial", 14, [System.Drawing.FontStyle]::Bold)
+
+$folderBrowserButton = New-Object System.Windows.Forms.Button
+$folderBrowserButton.Text = "Choose Folder"
+$folderBrowserButton.Location = New-Object System.Drawing.Point(100, 80)
+$folderBrowserButton.Width = 200
+$folderBrowserButton.Font = New-Object System.Drawing.Font("Arial", 12, [System.Drawing.FontStyle]::Regular)
+$folderBrowserButton.Add_Click({
+    $openFolderDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+    $openFolderDialog.Description = "Select the source directory"
+    $result = $openFolderDialog.ShowDialog()
+
+    if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
+        $selectedFolder = $openFolderDialog.SelectedPath
+        Write-Host "Selected Folder: $selectedFolder"
+        $folderPath = $openFolderDialog.SelectedPath
 
     $files = Get-ChildItem -Path $folderPath -File
 
@@ -38,11 +65,17 @@ if ($dialogResult -eq [System.Windows.Forms.DialogResult]::OK) {
                 Write-Host "File $($file.Name) does not contain a year."
             }
         }
+        Show-SuccessDialog
+        $form.Close()
     } else {
-        Write-Host "No files found in the folder."
+        Show-ErrorDialog
+        Write-Host "Error."
+        $form.Close()
     }
-} else {
-    Write-Host "No source directory selected."
-}
+    }
+})
 
-Read-Host -Prompt "Press Enter to exit"
+$form.Controls.Add($label)
+$form.Controls.Add($folderBrowserButton)
+
+$form.ShowDialog()
